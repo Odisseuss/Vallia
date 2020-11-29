3727596b53769fc85cb6e260aae371823fa24113
Rares <rares_lungescu@yahoo.com>
2020-11-29 17:55:07 +0200

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import "./App.css";
 import LandingWithSVGBackground from "./layout_presets/LandingWithSVGBackground";
 import LandingWithAnimatedText from "./layout_presets/LandingWithAnimatedText";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       {/* <LandingWithSVGBackground /> */}
+      <Navbar />
       <LandingWithAnimatedText />
     </div>
   );
