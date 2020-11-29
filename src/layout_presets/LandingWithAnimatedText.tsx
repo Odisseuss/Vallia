c03ed5e5006b438f365b9eda98ec513bb8c55ce4
Rares <rares_lungescu@yahoo.com>
2020-11-29 17:56:02 +0200

    Configure LandingPageWithAnimatedText

--- src/layout_presets/LandingWithAnimatedText.tsx
+++ src/layout_presets/LandingWithAnimatedText.tsx
@@ -7,6 +7,10 @@ import Paragraph from "../components/Paragraph";
 import { animations, colors } from "../ThemeConfig";
 import VictorImg from "../img/Victor.jpg";
 import { init } from "ityped";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Skills from "../components/Skills";
 
 function LandingWithAnimatedText() {
   React.useEffect(() => {
@@ -21,56 +25,71 @@ function LandingWithAnimatedText() {
     });
   }, []);
   return (
-    <Hero flex>
-      <CustomContainer
-        width="100%"
-        flex
-        flexDirection="row"
-        justifyContent="center"
-      >
-        <CustomContainer
-          width="80%"
-          flex
-          flexDirection="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <CustomContainer width="100%" flex>
-            <Paragraph fontSize="1.1rem" margin="0">
-              Hello. I am Victor Smith
-            </Paragraph>
-            <BigText
-              elementColor={colors.grey}
-              fontSize="4.5rem"
-              lineHeight="5rem"
-              margin="0 0 5rem 0"
-            >
-              I build{" "}
-              <span id="animatedText" style={{ color: colors.primary }}></span>
-              <br /> for the web.
-            </BigText>
-            <Button
-              whileHover={animations.button_filled_move_up}
-              borderRadius="50px"
-              fontColor={colors.navy_blue}
-              uppercase
-            >
-              Contact Me
-            </Button>
-          </CustomContainer>
-
-          <img
-            style={{
-              width: "30rem",
-              height: "33rem",
-              objectFit: "cover",
-              borderRadius: "15px",
-            }}
-            src={VictorImg}
-            alt=""
-          />
-        </CustomContainer>
-      </CustomContainer>
+    <Hero flex width="100%" height="100%">
+      <Container className="py-5">
+        <Row className="align-items-center py-lg-5">
+          <Col lg={8} className={"mb-5 mb-lg-0 py-lg-5"}>
+            <CustomContainer width="100%" flex>
+              <Paragraph fontSize="1.1rem" margin="0">
+                Hello. I am Victor Smith
+              </Paragraph>
+              <BigText
+                elementColor={colors.grey}
+                fontSize="4rem"
+                lineHeight="4.5rem"
+                margin="0 0 5rem 0"
+              >
+                I build {window.innerWidth < 650 ? <br /> : " "}
+                <span
+                  id="animatedText"
+                  style={{ color: colors.primary }}
+                ></span>
+                <br /> for the web.
+              </BigText>
+              <Button
+                whileHover={animations.button_filled_move_up}
+                borderRadius="50px"
+                fontColor={colors.navy_blue}
+                uppercase
+              >
+                Contact Me
+              </Button>
+            </CustomContainer>
+          </Col>
+          <Col lg={4} md={8} sm={9} className="py-lg-5">
+            <img
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                borderRadius: "15px",
+              }}
+              src={VictorImg}
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
+      <div style={{ height: 1000 }}></div>
+      <Skills
+        skills={[
+          {
+            completionPercentage: 30,
+            fillerColor: "#64FF8F",
+            skillName: "HTML",
+          },
+          {
+            completionPercentage: 70,
+            fillerColor: "#64FF8F",
+            skillName: "CSS",
+          },
+          {
+            completionPercentage: 90,
+            fillerColor: "#64FF8F",
+            skillName: "JavaScript",
+          },
+        ]}
+      />
     </Hero>
   );
 }
