264018291512870035f1fd4dd34dd37d82516ba1
Rares <rares_lungescu@yahoo.com>
2020-12-03 15:47:58 +0200

    Update CustomContainer to accept more styles from props

--- src/components/CustomContainer.tsx
+++ src/components/CustomContainer.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import styled from "styled-components";
+import { motion, HTMLMotionProps } from "framer-motion";
 
-interface ContainerProps {
+export interface ContainerProps extends HTMLMotionProps<"div"> {
   readonly height?: string;
   readonly width?: string;
+  readonly backgroundColor?: string;
   readonly margin?: string;
+  readonly border?: string;
+  readonly padding?: string;
+  readonly borderRadius?: string;
   readonly flex?: boolean;
   readonly flexDirection?: "column" | "row" | "row-reverse" | "column-reverse";
   readonly justifyContent?:
@@ -16,7 +21,8 @@ interface ContainerProps {
     | "space-evenly";
   readonly alignItems?: "center" | "start" | "end" | "stretch";
 }
-let StyledContainer = styled.div<ContainerProps>`
+let StyledContainer = styled(motion.div)<ContainerProps>`
+    background-color: ${(props) => props.backgroundColor ?? ""};
     height: ${(props) => props.height ?? ""};
     width: ${(props) => props.width ?? ""};
     ${(props) =>
@@ -28,6 +34,9 @@ let StyledContainer = styled.div<ContainerProps>`
           };\nalign-items: ${props.alignItems ?? "start"};`
         : ""}
     margin: ${(props) => props.margin ?? ""};
+    padding: ${(props) => props.padding ?? ""};
+    border-radius: ${(props) => props.borderRadius ?? ""};
+    border: ${(props) => props.border ?? ""};
   }
   `;
 
