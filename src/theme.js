c88b2be8fc694ddf793035542c2a20d3a287d0c9
Rares <rares_lungescu@yahoo.com>
2021-01-09 21:01:19 +0200

    Install theme-ui and create a dummy theme

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+let theme = {
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: '"Avenir Next", sans-serif',
+    monospace: "Menlo, monospace",
+  },
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "#33e",
+    greys: {
+      50: "#f7fafc",
+      300: "#CBD5E0",
+      600: "#4A5568",
+    },
+  },
+};
+
+export default theme;
