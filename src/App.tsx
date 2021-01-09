aeff84e73b4aa49c1e61c34ca6942e2dc555de7f
Rares <rares_lungescu@yahoo.com>
2021-01-09 21:01:50 +0200

    Add theme-ui provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
+import { ThemeProvider } from "theme-ui";
 import "./App.css";
+import Button from "./components/inputs/Button";
+import theme from "./theme";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Button>Button</Button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
